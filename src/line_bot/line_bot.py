2399cd61899157b58bcb261ff30cf5ce4816a7ca
tomowarkar <3104kawamoto@gmail.com>
2019-09-20 17:59:55 +0900

    リッチメニュー作った

--- src/line_bot/line_bot.py
+++ src/line_bot/line_bot.py
@@ -2,7 +2,7 @@ from linebot import LineBotApi
 from linebot.models import TextSendMessage
 from linebot.exceptions import LineBotApiError
 
-channel_token = "FgRzCbM/VsbxtRoGtRAUpykdJIMnV1UWeRkPqrdY3t5LQjOGI+4tXdIPao5HZufyrq24ZmlnzzS66Evhev9AZOCe2qIKZIxH+k4WJWnqo+Xm18ZR8LENzpBsKwGYJDZhkK+CE/ZTbJoQxI4jwRH6RQdB04t89/1O/w1cDnyilFU="
+channel_token = ""
 receivers = []
 
 line_bot_api = LineBotApi(channel_token)
