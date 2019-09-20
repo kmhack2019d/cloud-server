f89a67916f88785f1cdaf097de9682b230ead6e7
Saji Ayahito <ayahito_saji@outlook.jp>
2019-09-20 17:23:37 +0900

    deploy frontend app

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,11 +1,16 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', static_url_path='/', template_folder="static")
+
+@app.route('/api/v1/')
+def helloworld():
+    return jsonify({"hello": "world"})
 
 @app.route('/')
 def index():
-    return jsonify({"hello": "world"})
+    return render_template("index.html")
 
 if __name__ == '__main__':
+    print (app.url_map)
     app.debug = True
     app.run(host='0.0.0.0')
